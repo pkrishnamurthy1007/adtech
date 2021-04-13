5a0fa0ec1912554a7ba963da8fea2bb224b5f701
amal <aduriseti@gmail.com>
2021-04-12 18:57:38 -0700

    added table creation to sagemaker train entrypt

--- models/bing/time_of_day/tod_dp.py
+++ models/bing/time_of_day/tod_dp.py
@@ -102,7 +102,7 @@ def add_spline(df, index_col, smooth_col, spline_k, spline_s, postfix='_spline')
 def spread_outliers(S,percentile=97.5) -> typing.Iterable:
     OUTTHRESH = np.percentile(S,percentile)
     OUTI = S > OUTTHRESH
-    print("outlier thresh:", OUTTHRESH)
+    # print("outlier thresh:", OUTTHRESH)
     T = OUTI * OUTTHRESH + (1-OUTI) * S
     T = (S.sum() / T.sum()) * T
     assert abs(T.sum() - S.sum()) < 1e-10
@@ -190,49 +190,105 @@ def interval_fit_transform(X, W, nintervals, xapprox):
     for lb, ub in zip(interval_bounds[:-1], interval_bounds[1:]):
         Xapprox[lb:ub] = xapprox(X,W,lb,ub)
     return Xapprox
+
+def get_interval_modifier_table(product):
+    df_tz_adj = hc_quarter_hour_tz_adjusted(
+        start_date=start_date_ymd, end_date=end_date_ymd, product=product, traffic_source=BING)
+    df_tz_adj = compute_cema_tod_modifiers(df_tz_adj,window)
+    df_tz_adj = df_tz_adj.reset_index().set_index("dayofweek")
+    modifier_rows = []
+    for day in range(7):
+        X = df_tz_adj.loc[day, "cema_modifier"].values
+        W = df_tz_adj.loc[day, "sessions_cema"].values
+        
+        _, interval_bounds = interval_fit(X, W, BING_DAILY_INTERVALS, wavgapprox)
+        interval_bounds = [0, *interval_bounds]
+        interval_hr_bounds = df_tz_adj.loc[day,"hourofday"].iloc[interval_bounds[:-1]]
+        interval_hr_bounds = [*interval_hr_bounds,24]
+        intervals = [*zip(interval_hr_bounds[:-1],interval_hr_bounds[1:])]
+        interval_modifiers = [wavgapprox(X,W,lb,ub) for lb,ub in zip(interval_bounds[:-1],interval_bounds[1:])]
+        DAYS = ["SUN","MON","TUE","WED","THR","FRI","SAT"]
+        modifier_rows += [{
+            "product": product,
+            "weekday_index": day,
+            "weekday": DAYS[day], 
+            "hr_start_inclusive": start_hr,
+            "hr_end_exclusive": end_hr,
+            "modifier": mod
+            } for (start_hr,end_hr),mod in zip(intervals,interval_modifiers)]
+    return pd.DataFrame(modifier_rows)
 #%%
-NOW = datetime.datetime.now()
-DAY = datetime.timedelta(days=1)
+TABOOLA                 = "TABOOLA"
+MEDIA_ALPHA             = "MEDIAALPHA"
+BING                    = "BING"
+U65                     = "HEALTH"
+O65                     = 'MEDICARE'
+BING_DAILY_INTERVALS    = 7
 
-start_date = NOW - 90*DAY
-end_date = NOW - 0*DAY
-window = 16
+if __name__ == "__main__":
+    NOW = datetime.datetime.now()
+    DAY = datetime.timedelta(days=1)
 
-start_date_ymd = start_date.strftime("%Y%m%d")
-end_date_ymd = end_date.strftime("%Y%m%d")
-product = None
-traffic_source = None
-start_date_ymd,end_date_ymd
+    start_date = NOW - 90*DAY
+    end_date = NOW - 0*DAY
+    window = 16
 
-TABOOLA     = "TABOOLA"
-MEDIA_ALPHA = "MEDIAALPHA"
-BING        = "BING"
-U65         = "HEALTH"
-O65         = 'MEDICARE'
-PRODUCT     = O65
+    start_date_ymd = start_date.strftime("%Y%m%d")
+    end_date_ymd = end_date.strftime("%Y%m%d")
 
-df_tz_adj = hc_quarter_hour_tz_adjusted(
-    start_date=start_date_ymd, end_date=end_date_ymd, product=PRODUCT, traffic_source=BING)
-df_tz_adj = compute_cema_tod_modifiers(df_tz_adj,window)
-DAILY_INTERVALS = 7
-df_tz_adj = df_tz_adj.reset_index().set_index("dayofweek")
-df_tz_adj["cema_modifier_interval"] = np.NaN
-for day in range(7):
-    X = df_tz_adj.loc[day, "cema_modifier"].values
-    W = df_tz_adj.loc[day, "sessions_cema"].values
-    _,intervals = interval_fit(X, W, DAILY_INTERVALS, wavgapprox)
-    Xapprox = interval_fit_transform(X, W, DAILY_INTERVALS, wavgapprox)
-    df_tz_adj.loc[day, "cema_modifier_interval"] = Xapprox
+    bing_modifiers = pd.concat((
+        get_interval_modifier_table(U65),
+        get_interval_modifier_table(O65)
+    ))
+    #%%
+    bing_modifiers["calculation_date"] = NOW
+
+    SCHEMA = "data_science"
+    BING_TOD_MODIFIER_TABLE = "bing_tod_modifiers"
+    table_creation_sql = f"""
+        CREATE TABLE IF NOT EXISTS 
+        {SCHEMA}.{BING_TOD_MODIFIER_TABLE}
+        (
+            "product" VARCHAR(50),
+            "weekday_index" INT,
+            "weekday" VARCHAR(50),
+            "hr_start_inclusive" FLOAT,
+            "hr_end_exclusive" FLOAT,
+            "modifier" FLOAT,
+            "calculation_date" DATETIME
+        );
+    """
+
+    with HealthcareDW() as db:
+        db.exec(table_creation_sql)
+        db.load_df(bing_modifiers,schema=SCHEMA,table=BING_TOD_MODIFIER_TABLE)
+
+    def fit_eval_modfifiers(product):
+        df_tz_adj = hc_quarter_hour_tz_adjusted(
+            start_date=start_date_ymd, end_date=end_date_ymd, product=product, traffic_source=BING)
+        df_tz_adj = compute_cema_tod_modifiers(df_tz_adj,window)
+        df_tz_adj = df_tz_adj.reset_index().set_index("dayofweek")
+        df_tz_adj["cema_modifier_interval"] = np.NaN
+        for day in range(7):
+            X = df_tz_adj.loc[day, "cema_modifier"].values
+            W = df_tz_adj.loc[day, "sessions_cema"].values
+            Xapprox = interval_fit_transform(X, W, BING_DAILY_INTERVALS, wavgapprox)
+            df_tz_adj.loc[day, "cema_modifier_interval"] = Xapprox
+
+        df_tz_adj["sessions_cema_mean_adjusted"] = \
+            df_tz_adj["sessions_cema"] / df_tz_adj["sessions_cema"].mean()
+        rps_mean = (df_tz_adj["rps"] * df_tz_adj["sessions"]).sum() / df_tz_adj["sessions"].sum()
+        df_tz_adj["rps_mean_adjusted"] = spread_outliers(df_tz_adj["rps"]) / rps_mean
+        ax = df_tz_adj.reset_index().plot.scatter(x='int_ix', y='rps_mean_adjusted')
+        df_tz_adj.reset_index().set_index("int_ix")[["sessions_cema_mean_adjusted", "baseline", "cema_modifier", "cema_modifier_interval"]].plot(
+            ax=ax, figsize=(15, 5))
+        ax.set_title(f"Bid modifiers for product={product}")
+
+        return df_tz_adj
+
+    fit_eval_modfifiers(U65)
+    fit_eval_modfifiers(O65)
 #%%
-df_tz_adj["sessions_cema_mean_adjusted"] = \
-    df_tz_adj["sessions_cema"] / df_tz_adj["sessions_cema"].mean()
-rps_mean = (df_tz_adj["rps"] * df_tz_adj["sessions"]).sum() / df_tz_adj["sessions"].sum()
-df_tz_adj["rps_mean_adjusted"] = spread_outliers(df_tz_adj["rps"]) / rps_mean
-ax = df_tz_adj.reset_index().plot.scatter(x='int_ix', y='rps_mean_adjusted')
-df_tz_adj.reset_index().set_index("int_ix")[["sessions_cema_mean_adjusted", "baseline", "cema_modifier", "cema_modifier_interval"]].plot(
-    ax=ax, figsize=(15, 5))
-ax.set_title(f"Bid modifiers for product={PRODUCT}")
-
 # #%%
 # interval_fit(X,W,DAILY_INTERVALS,wavgapprox)
 # df = pd.DataFrame(data=mem.values(),index=pd.MultiIndex.from_tuples(mem.keys()))
