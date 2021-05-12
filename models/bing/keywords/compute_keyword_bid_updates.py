37e584190cdb2599188606a2b295f83344ac41b4
amal <aduriseti@gmail.com>
2021-05-12 07:35:10 -0700

    fixed paths

--- models/bing/keywords/compute_keyword_bid_updates.py
+++ models/bing/keywords/compute_keyword_bid_updates.py
@@ -486,33 +486,3 @@ for accnt in df_out.index.unique("account"):
         "hc-data-lake-storage",
         f"prod/data-science/bing-keyword-bids/{accnt}/{bids_fnm}")
 #%%
-df_check = df_out.copy()
-df_check["change"] = df_check["max_cpc_new"]/df_check["max_cpc_old"] - 1
-df_check["cost_delta_est"] = df_check["cost_y"] * df_check["change"]
-# assume rpc will be unchanged - but volume will decrease proportional
-#   to bid change
-df_check["rev_delta_est"] = df_check["rev_y"] * df_check["change"]
-df_check["roas"] = df_check["rev_y"] / df_check["cost_y"]
-# %%
-total_roas = df_check["rev_y"].sum() / df_check["cost_y"].sum()
-total_cost_delta_est = df_check["cost_delta_est"].sum()
-total_rev_delta_est = df_check["rev_delta_est"].sum()
-total_roas,total_cost_delta_est,total_rev_delta_est
-# %%
-df_check[["rev_y","cost_y","cost_delta_est","rev_delta_est"]].sum()
-# %%
-df_check[["cost_delta_est", "rev_delta_est"]].sum() / \
-    df_check[["rev_y", "cost_y"]].sum().values
-# %%
-total_roas
-#%%
-ROI_TARGET
-#%%
-"""
-u would expect REV chnage ~= (ROAS - ROI_TARGET)
-- |COST change| > | REV change | 
-
-"""
-#%%
-if total_roas > ROI_TARGET: pass
-#%%
