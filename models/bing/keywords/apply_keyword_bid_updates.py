053b20e6e8e9aeb2f576fafbd542c1e69fbaefd9
amal <aduriseti@gmail.com>
2021-05-14 09:49:44 -0700

    first pass at kw bid updates

--- models/bing/keywords/apply_keyword_bid_updates.py
+++ models/bing/keywords/apply_keyword_bid_updates.py
@@ -1,2 +1,84 @@
-import sys
-sys.exit(2)
+#%%
+import json
+import logging
+import os
+import glob
+import datetime
+import pandas as pd
+from api.bingads.bingapi.client import BingClient
+from models.bing.keywords.common import *
+
+bing_creds = json.loads(os.getenv("BING_CREDS"))
+
+# from ds_utils.db.connectors import HealthcareDW,AnalyticsDB
+# with AnalyticsDB() as db:
+#     bing_accnt_df = db.to_df("select * from dev_ent_d1_gold.adtech.bingads.account")
+# bing_accnt_df
+bing_accnt_df = pd.DataFrame(
+    [
+        ["B013P57C", "43030867"],
+        ["X000EWRC", "3196099"],
+        ["B013D68T", "43060164"],
+    ],
+    columns=["account_number", "account_id"],
+)
+
+# TODAY = datetime.datetime.now().date()
+TODAY = "2021-05-12"
+todays_output = glob.glob(f"{OUTPUT_DIR}/**/*{TODAY}.csv")
+df_out = pd.concat((pd.read_csv(fpth) for fpth in todays_output))
+#%%
+for accnt in df_out["account"].unique():
+    print(f"Updating bids for account# `{accnt}`")
+    accnt_id = bing_accnt_df.set_index("account_number").loc["B013P57C","account_id"]
+    print(f"Got id `{accnt_id}` for account# `{accnt}`")
+    accnt_client = client = BingClient(
+        account_id=accnt_id,
+        customer_id=bing_creds['BING_CUSTOMER_ID'],
+        dev_token=bing_creds['BING_DEVELOPER_TOKEN'],
+        client_id=bing_creds['BING_CLIENT_ID'],
+        refresh_token=bing_creds['BING_REFRESH_TOKEN']
+    )
+    accntI = df_out["account"] == accnt
+
+    import tqdm
+    adgps = df_out.loc[accntI, "adgroup_id"].unique()
+    print(f"got {len(adgps)} adgroups for account# {accnt}")
+    for adgp in tqdm.tqdm(adgps):
+        adgpI = df_out["adgroup_id"] == adgp
+        keyword_ids, keyword_bids = df_out \
+            .loc[accntI & adgpI, ['keyword_id', "max_cpc_old"]].values.T
+        response = client.update_keyword_bids(
+            adgp, [*keyword_ids.astype(int)], [*keyword_bids.astype(float)])
+        break
+        # print(f"Adgroup {adgroup} success: {response}")
+#%%
+type(adgp)
+#%%
+keyword_ids.astype(int),keyword_bids
+#%%
+key
+#%%
+
+client = BingClient(
+    account_id=bing_accnt_df.set_index("account_number").loc["B013P57C","account_id"],
+    # account_id=credentials['BING_ACCOUNT_ID'],
+    customer_id=bing_creds['BING_CUSTOMER_ID'],
+    dev_token=bing_creds['BING_DEVELOPER_TOKEN'],
+    client_id=bing_creds['BING_CLIENT_ID'],
+    refresh_token=bing_creds['BING_REFRESH_TOKEN']
+)
+
+
+adgroups = df['adgroup_id'].unique().tolist()
+
+for adgroup in adgroups:
+    temp_df = df[df['adgroup_id'] == adgroup]
+    keyword_ids = temp_df['keyword_id'].tolist()
+    keyword_bids = temp_df['bid'].tolist()
+
+    response = client.update_keyword_bids(
+        adgroup, keyword_ids, keyword_bids)
+    print(f"Adgroup {adgroup} success: {response}")
+
+#%%
--- workspace/nb_taboola.py
+++ workspace/nb_taboola.py
@@ -1,13 +1,13 @@
 #%%
 # hc creds
 hccreds = {
-    "client_id": "3cc90f549fd44a9fbe36f70aa6e7cfde",
-    "client_secret": "eaa74a94108648579e65167bf276c3a1",
+  "client_id": "3cc90f549fd44a9fbe36f70aa6e7cfde",
+  "client_secret": "eaa74a94108648579e65167bf276c3a1",
 }
 # pivot creds
 pivotcreds = {
-    "client_id": "31d7f068eb3d43acb85b5ad64eef2ca9",
-    "client_secret": "c5010044df194010817e76edac0fd426",
+  "client_id": "31d7f068eb3d43acb85b5ad64eef2ca9",
+  "client_secret": "c5010044df194010817e76edac0fd426",
 }
 
 from pytaboola import TaboolaClient
@@ -32,12 +32,12 @@ O65_ACCNT_ID = "taboolaaccount-rangaritahealthcarecom"
 id2accnt = {a["account_id"]: a for a in accnts}
 
 def accnt_camps(accnt):
-    camp_service = CampaignService(client, accnt["account_id"])
-    return camp_service.list()
+  camp_service = CampaignService(client, accnt["account_id"])
+  return camp_service.list()
 aid2cid2camp = {}
 for aid,a in tqdm.tqdm(id2accnt.items()):
-    cid2camp = {c["id"]: c for c in accnt_camps(a)}
-    aid2cid2camp[aid] = cid2camp
+  cid2camp = {c["id"]: c for c in accnt_camps(a)}
+  aid2cid2camp[aid] = cid2camp
 
 import json
 json.dump(aid2cid2camp, open("camps.json", "w"))
@@ -50,47 +50,131 @@ import jmespath
 get = jmespath.search
 
 active_camps = get(
-    "*.*[] | [?is_active]",
-    aid2cid2camp,
+  "*.*[] | [?is_active]",
+  aid2cid2camp,
 )
 print("|active campaigns|:",len(active_camps))
 #%%
-1/0
 o65_camp_svc = CampaignService(client,O65_ACCNT_ID)
 test_camp_svc = CampaignService(client,TEST_ACCNT_ID)
 test_accnt_camps = aid2cid2camp[TEST_ACCNT_ID].values()
 test_accnt_camp_names = {c["name"] for c in test_accnt_camps}
 print("|test_accnt_camps|:",len(test_accnt_camps))
-taboola_req_fields = [
+def copy_campaign_base(camp,to=TEST_ACCNT_ID):
+  taboola_req_fields = [
     "name","branding_text","cpc","spending_limit",
     "spending_limit_model","marketing_objective"]
+  camp_svc = CampaignService(client,to)
+  camp_base = {f: camp[f] for f in taboola_req_fields}
+  camp_copy = camp_svc.create(**camp_base)
+  print(f"created base for `{camp['name']}` in account `{to}` w/ id:", camp_copy["id"])
+  camp_copy = camp_svc.update(camp_copy["id"], is_active=False)
+  assert camp_copy["is_active"] == False
+  print("...succesfully deactivated newly created campaign")
+  return camp_copy
 for camp in active_camps:
-    # skip already created test account campaigns
-    if camp["name"] in test_accnt_camp_names: 
-        print(f"Campaign `{camp['name']}` already exists in account `{TEST_ACCNT_ID}`")
-    camp_base = {f: camp[f] for f in taboola_req_fields}
-    camp_copy = test_camp_svc.create(**camp_base)
-    print(f"created base for `{camp['name']}` in account `{TEST_ACCNT_ID}` w/ id:", camp_copy["id"])
-    updated_camp = test_camp_svc.update(camp_copy["id"], is_active=False)
-    assert updated_camp["is_active"] == False
-    print("...succesfully deactivated newly created campaign")
+  # skip already created test account campaigns
+  if camp["name"] in test_accnt_camp_names: 
+    print(f"Campaign `{camp['name']}` already exists in account `{TEST_ACCNT_ID}`")
+  else:
+    camp_copy = copy_campaign_base(camp,to=TEST_ACCNT_ID)
 #%%
+#%%
+[c["status"] for c in test_accnt_camps]
+#%%
+def get_camp_by_name(name,accnt=TEST_ACCNT_ID):
+  # TODO: figure out some way to avoid having to refetch all campaigns
+  #       => will be able to use catalog tables eventually
+  camp_svc = CampaignService(client, accnt)
+  camps = camp_svc.list()
+  match_camps = [c for c in camps if c["name"] == name]
+  assert len(match_camps) == 1, (name,len(match_camps))
+  return match_camps[0]
 import pytaboola
 pytaboola.errors.TaboolaError
-#%%
 import traceback
-for k,attr in camp.items():
-    print(f"attempting to copy over field `{k}`...")
+import tqdm
+field_update_attempts = {}
+field_update_errors = {}
+def update_fields(camp,accnt,camp_id):    
+  camp_svc = CampaignService(client, accnt)
+  for k,attr in tqdm.tqdm([*camp.items()]):
+    # print(f"attempting to copy over field `{k}`...")
     try:
-        test_camp_svc.update(updated_camp["id"],**{k: attr})
-        print("...Sucess!")
+      camp_svc.update(camp_id,**{k: attr})
+      field_update_attempts.setdefault(k,[]).append(1)
+      # print("...Sucess!")
     except (Exception,pytaboola.errors.TaboolaError) as ex:
-        print(f"Failed w/ {type(ex)}:{ex}\n{traceback.format_exc()}")
+      errd = {
+        "ex": ex,
+        "type": type(ex),
+        "str": str(ex),
+        "tb": traceback.format_exc()
+      }
+      # print(f"Failed w/ {type(ex)}:{ex}\n{traceback.format_exc()}")
+      # print(f"Failed w/ {type(ex)}:{ex}")
+      field_update_attempts.setdefault(k, []).append(0)
+      field_update_attempts.setdefault(k, []).append(errd)
+
+for camp in active_camps:
+  dest_camp = get_camp_by_name(camp["name"])
+  dest_id = dest_camp["id"]
+  print("UPDATEING",camp["name"],dest_id)
+  update_fields(camp,TEST_ACCNT_ID,dest_id)
+
+update_attempt_df = pd.DataFrame(field_update_attempts)
+update_attempt_df.mean(axis=0)
+#%%
+"""
+TODO: first taboola test
+- what to test?
+  1. time of day modifiere
+  2. location modifiers
+  3. publisher modifiers
+- testing infrastructure
+  - different accounts for A/B groups? - would allow easier test management
+    - these endpts may be useful for bulk updates of `is_active` and `location_targetting`:
+      https://developers.taboola.com/backstage-api/reference#bulk-update-campaigns
+  - flag in campaign catalog table indicating what group the campaign belogns to
+- duplicate campaigns in test account: https://developers.taboola.com/backstage-api/reference#duplicate-a-campaign
+  - refreshed via hourly or 1/2/4/6/8/12x daily ETL 
+    -figure out what fields are writable and write all of those fields
+    - NOTE: think this may not pick up stuff like `audience_targetting`
+    - also - was getting errors writing our publisher targetting
+      - may have been timeout errors - but need to check       
+  - how are chagnes propagated when we have many campaigns?
+    - probably dont want to use name to indicate structure
+    - maybe some kind of tree structure in catalog tables
+    - can skip for now
+- cammpaign management daemon
+  - only needed if we want to do TOD modifiers or on/off testing
+- modifier script
+  - compute:
+    - publisher modifier - already written
+    - location modifiers - very similar to publisher modifier 
+    - time-of-day modfiiers 
+    - run daily or weekly
+  - validate:
+    - TODO - sync w/ Alexa
+  - apply
+    - TODO
+    - should this be done by Dan?
+    - might have to consider catalog tables and A/B flags 
+    - for TOD modifiers would be hitting a table consumed by campaign management daemon
+- testing procedure
+  - test setup
+    - create duplicate campaigns and sets up ETL to maintain them
+    - marks cmapaigns as A or B in catalog tables
+    - sets up scheduled modifier script
+    - maybe makes some entry in an A/B table
+  - test-teardown
+    - ?
+"""
 #%%
 #%%
 from pytaboola.services.report import \
-    CampaignSummaryReport, RecirculationSummaryReport, \
-    TopCampaignContentReport, RevenueSummaryReport, VisitValueReport
+  CampaignSummaryReport, RecirculationSummaryReport, \
+  TopCampaignContentReport, RevenueSummaryReport, VisitValueReport
 import datetime
 
 DAY = datetime.timedelta(days=1)
