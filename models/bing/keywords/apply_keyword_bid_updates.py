593a4169fb4711d00feef49e8ba60c10b90cbbb8
amal <aduriseti@gmail.com>
2021-05-14 17:11:05 -0700

    fixed env var

--- models/bing/keywords/apply_keyword_bid_updates.py
+++ models/bing/keywords/apply_keyword_bid_updates.py
@@ -33,14 +33,21 @@ assert df_out.__len__() * 2 == old_len, """
 We should have 2 records for each kw b/c we break bids out by account and write them,
 but we also write the bids for all accounts.
 """
-#%%
-# # Debug requests and responses
-# logging.basicConfig(level=logging.INFO)
+
+# Debug requests and responses
+LOGLEVEL = logging.WARN
+logging.basicConfig(level=LOGLEVEL)
 # logging.getLogger('suds.client').setLevel(logging.DEBUG)
 # logging.getLogger('suds.transport.http').setLevel(logging.DEBUG)
-import sys
-sys.exit(0)
-#%%
+
+# import sys
+# sys.exit(0)
+
+DRY=True
+def get_kw_bids(adgp,keyword_ids):
+    kws = accnt_client.get_keywords(adgp,keyword_ids)
+    kw_bids = {kw["Id"]: kw.Bid.Amount for kw in kws["Keywords"]["Keyword"]}
+    return kw_bids
 for accnt in df_out["account"].unique():
     print(f"Updating bids for account# `{accnt}`")
     accnt_id = bing_accnt_df \
@@ -51,7 +58,8 @@ for accnt in df_out["account"].unique():
         customer_id=bing_creds['BING_CUSTOMER_ID'],
         dev_token=bing_creds['BING_DEVELOPER_TOKEN'],
         client_id=bing_creds['BING_CLIENT_ID'],
-        refresh_token=bing_creds['BING_REFRESH_TOKEN']
+        refresh_token=bing_creds['BING_REFRESH_TOKEN'],
+        loglevel=LOGLEVEL,
     )
     accntI = df_out["account"] == accnt
 
@@ -61,7 +69,15 @@ for accnt in df_out["account"].unique():
     for adgp in tqdm.tqdm(adgps):
         adgpI = df_out["adgroup_id"] == adgp
         keyword_ids, keyword_bids = df_out \
-            .loc[accntI & adgpI, ['keyword_id', "max_cpc_old"]].values.T
+            .loc[accntI & adgpI, ['keyword_id', "max_cpc_new"]].values.T
+        keyword_ids = [*keyword_ids.astype(int)]
+        keyword_bids = [*keyword_bids.astype(float)]
+        old_kw_bids = get_kw_bids(adgp, keyword_ids)
         response = accnt_client.update_keyword_bids(
-            adgp, [*keyword_ids.astype(int)], [*keyword_bids.astype(float)])
+            adgp, keyword_ids, keyword_bids, dry=DRY)
+        new_kw_bids = get_kw_bids(adgp, keyword_ids)
+        if DRY:
+            assert new_kw_bids==old_kw_bids, (old_kw_bids, new_kw_bids)
+        else:
+            assert new_kw_bids==dict(zip(keyword_ids,keyword_bids)),(new_kw_bids,keyword_ids,keyword_bids)
 #%%
