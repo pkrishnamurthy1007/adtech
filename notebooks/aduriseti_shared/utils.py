66e5237d7bc221eecde5abe4d268a6587686516d
amal <aduriseti@gmail.com>
2021-05-25 16:07:32 -0700

    moving worksapce to shared notebooks folder

---
+++ notebooks/aduriseti_shared/utils.py
@@ -0,0 +1,10 @@
+from pkg_resources import resource_filename as rscfn
+import os
+from ds_utils.db.connectors import HealthcareDW,PivotDW
+TABLE_DUMPS = rscfn(__name__,"TABLE_DUMPS")
+os.makedirs(TABLE_DUMPS,exist_ok=True)
+def fetch_head(rsc,limit=1000,src=HealthcareDW):
+    with src() as db:
+        db \
+            .to_df(f"select * from {rsc} limit {limit}") \
+            .to_csv(f"{TABLE_DUMPS}/{src.__name__}.{rsc}.csv")
