28fd334813e3f58c10ce9e3f2e9cf186ddcf4c8e
amal <aduriseti@gmail.com>
2021-07-20 14:23:32 -0700

    added underscore in trigger file for function not intented to be a DAG element

--- .github/workflows/airflow_triggers.py
+++ .github/workflows/airflow_triggers.py
@@ -5,7 +5,7 @@ OWNER = "healthcarecom"
 REPO = "adtech"
 PAT = "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"
 
-def trigger_workflow(workflow_id,kwargs):
+def _trigger_workflow(workflow_id,kwargs):
     resp = requests.post(
         f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{workflow_id}/dispatches",
         json={
@@ -22,7 +22,7 @@ def trigger_workflow(workflow_id,kwargs):
     return resp
 
 def bing_update_bids_keyword(execution_date):
-    return trigger_workflow(
+    return _trigger_workflow(
         "bing_update_bids_keyword.yml",
         {
             "execution_date": execution_date
@@ -30,7 +30,7 @@ def bing_update_bids_keyword(execution_date):
     )
 
 def bing_update_bids_tod(execution_date):
-    return trigger_workflow(
+    return _trigger_workflow(
         "bing_update_bids_tod.yml",
         {
             "execution_date": execution_date
@@ -38,7 +38,7 @@ def bing_update_bids_tod(execution_date):
     )
 
 def taboola_update_bids(execution_date):
-    return trigger_workflow(
+    return _trigger_workflow(
         "taboola_update_bids.yml",
         {
             "execution_date": execution_date
