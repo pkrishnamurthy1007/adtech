9f4d7c31ee34ce74483ed09350e0deff8eaba36e
amal <aduriseti@gmail.com>
2021-07-20 07:02:17 -0700

    config change

--- models/bing/keywords/config.py
+++ models/bing/keywords/config.py
@@ -1,5 +1,7 @@
 ROI_TARGET = 1.15  # target we are aiming
 CLICKS = 120  # click threshold. level at which kw uses all of its own data
-MAX_PUSH = 0.2
-MAX_CUT = -0.3
+# MAX_PUSH = 0.2
+# MAX_CUT = -0.3
+MAX_PUSH = 0.5
+MAX_CUT = -0.5
 CPC_MIN = 0.05
