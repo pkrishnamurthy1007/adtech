bbb50dd30bfaa27e90888a6547dd6296b88a4bf1
amal <aduriseti@gmail.com>
2021-07-20 14:21:45 -0700

    cleaned up trigger file"

--- .github/workflows/airflow_triggers.py
+++ .github/workflows/airflow_triggers.py
@@ -1,4 +1,3 @@
-#%%
 import requests
 import datetime
 
@@ -45,27 +44,3 @@ def taboola_update_bids(execution_date):
             "execution_date": execution_date
         }
     )
-#%%
-# import requests
-# import datetime
-# """
-# https://api.github.com/repos/octocat/hello-world/actions/workflows/42/dispatches \
-# """
-# OWNER = "healthcarecom"
-# REPO = "adtech"
-# WORKFLOW_ID = "bing_update_bids_keyword.yml"
-# PAT = "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"
-# resp = requests.post(
-#     f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches",
-#     json={
-#         "ref": "workspace_amal",
-#         "inputs": {
-#             "execution_date": datetime.datetime.now().date().__str__()  
-#         }
-#     },
-#     headers={
-#         'Authorization': f'token {PAT}'
-#     },
-# )
-# resp.raise_for_status()
-# #%%
